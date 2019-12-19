3023a62d6b246fd63c19e2e545d9f49ee1eaa46a
LiangJieMr <13453796194@163.com>
2019-12-19 17:17:00 +0800

    'json'

--- html2canvas/index.js
+++ html2canvas/index.js
@@ -1,8 +1,8 @@
 /**
  * @file: index.js
  * @author: 梁杰
- * @data: 2019/12/19 10:22
- * @Description html2canvas
+ * @data: 2019/12/19 14:59
+ * @Description
  */
 //获取截屏
 var Screenshots = document.querySelector('.Screenshots');
@@ -14,12 +14,14 @@ var a = document.querySelector('.a');
 
 // 创建一个canvas
 var canvas2 = document.querySelector(".canvas");
-let canvasObj = document.querySelector('.test');
+let canvasObj = document.querySelector('.test1');
 var w = parseInt(window.getComputedStyle(canvasObj).width);   //获取目标元素的宽高
 var h = parseInt(window.getComputedStyle(canvasObj).height);
 //将canvas画布放大若干倍，然后盛放在较小的容器内，就显得不模糊了
 var context = canvas2.getContext("2d");
-context.scale(2, 2);
+context.scale(1,1 );
+context.translate('-'+(canvasObj.offsetLeft),'-'+(canvasObj.offsetTop))
+
 canvas2.width = w;
 canvas2.height = h;
 canvas2.style.width = w + 'px';
@@ -28,7 +30,7 @@ canvas2.style.transition = "transform 0.3s cubic-bezier(0.42, 0, 0.58, 1) 0s, op
 
 
 // 完整下载
-// html2canvas(document.querySelector('.test'), { canvas: canvas2 }).then(function(canvas) {
+// html2canvas(document.querySelector('.test1'), { canvas: canvas2 }).then(function(canvas) {
 //     const a = document.createElement("a");
 //     // 文件名
 //     a.download = name | 'html2canvas';
@@ -42,7 +44,7 @@ canvas2.style.transition = "transform 0.3s cubic-bezier(0.42, 0, 0.58, 1) 0s, op
  * 截屏方法
  */
 function takeScreenshot(){
-    html2canvas(document.querySelector('.test'),{
+    html2canvas(document.querySelector('.test1'),{
         canvas: canvas2,
         background: 'red'
     }).then(function (canvas) {
@@ -60,20 +62,19 @@ function takeScreenshot(){
         document.body.appendChild(canvas)
     })
 }
-// html2canvas(document.querySelector('.test'),{ canvas: canvas2 }).then(function (canvas) {
- //  var imgData = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
 
-    // window.location.href= imgData;
-    // let image = new Image();
-    // image.className = "newImg";
-    // image.src = canvas.toDataURL("image/png");
+// 图片
+ /*html2canvas(document.querySelector('.test'),{ canvas: canvas2 }).then(function (canvas) {
+     let image = new Image();
+     image.className = "newImg";
+     image.src = canvas.toDataURL("image/png");
 
 //     // 设置 a href 属性
-//     a.download = name | 'html2canvas';
-//     a.setAttribute("href", canvas.toDataURL("image/png"));
-//     document.body.appendChild(canvas)
+    a.download = name | 'html2canvas';
+    a.setAttribute("href", canvas.toDataURL("image/png"));
+    document.body.appendChild(image)
     // document.querySelector('.app').appendChild(canvas)
-// });
+});*/
 
 /*html2canvas(canvasObj, {
     onrendered: function (canvas) {
@@ -83,7 +84,7 @@ function takeScreenshot(){
 });*/
 // 文件名称有误
 /*
-html2canvas(document.querySelector('.test'), {
+html2canvas(document.querySelector('.test1'), {
     onrendered: function(canvas) {
         var url = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");//图片地址
         document.body.appendChild(canvas);
